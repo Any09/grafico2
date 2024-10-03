766ec363f274c518d32b451b2d87192c3eb49522
Anny <104777994+Any09@users.noreply.github.com>
2024-10-03 15:08:22 -0300

    fff

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -4,8 +4,8 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = ["1531-1575","1576-1600","1601-1625","1626-1650","1651-1670","1701-1710","1711-1720","1721-1730","1731-1740","1741-1750","1751-1760","1761-1770","1771-1780"]
-    const quantidadeUsuarios = ["10000","40000","100000","100000","185000","175000","156700","139000","146300","166100","185100","169400","164600","161300","1895500"]
+    const nomeDasRedes = ["Argélia","Sudão","Egito","Etiópia","Mauritânia"]
+    const quantidadeUsuarios = ["44903225","46874204","110.990.103","123379924","4.736.139"]
     const data = [
         {
             x: nomeDasRedes,
